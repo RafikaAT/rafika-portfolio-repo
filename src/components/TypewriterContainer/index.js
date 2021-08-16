07771d563a80e74f5b701c171db1ed315bc6642f
Rafika <RTuremis@hotmail.co.uk>
2021-08-16 08:55:09 +0100

    Import pic and use accordingly in TypewriterContainer

--- src/components/TypewriterContainer/index.js
+++ src/components/TypewriterContainer/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Typewriter from 'typewriter-effect';
 import './style.css';
+import myImage from '../../images/intro-image.jpeg';
 
 // Need to set loop for continuous typewriting
 const TypewriterContainer = () => {
   return (
     <div className="typewriter">
       <div className="Rafika-image">
-        <img id="introductory-image" src="../../images/intro-image.jpeg" />
+        <img id="introductory-image" src={myImage} />
       </div>
       <div className="Rafika-introduction-container">
         <h1 id="Greeting">Hi, I'm Rafika</h1>
