ac270a440303a9d15f31a2de589cc8180fbe8574
Rafika <RTuremis@hotmail.co.uk>
2021-08-21 22:46:53 +0100

    Initial project page setup complete

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,16 +1,56 @@
 import React from 'react';
-import { ConstructionNotice } from '../../components';
+// import { ConstructionNotice } from '../../components';
 import './style.css';
+import journelImage from '../../images/journel.png';
+import habitualImage from '../../images/habitual.png';
+import quizzbuzzImage from '../../images/quizzbuzz.png';
+import weatherAppImage from '../../images/weather.png';
 
 const Projects = () => {
+  const projectArray = [
+    {
+      id: 1,
+      name: 'Journel',
+      image: journelImage,
+      para:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    },
+    {
+      id: 2,
+      name: 'Habitual',
+      image: habitualImage,
+      para:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    },
+    {
+      id: 3,
+      name: 'QuizzBuzz',
+      image: quizzbuzzImage,
+      para:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    },
+    {
+      id: 4,
+      name: 'WeatherApp',
+      image: weatherAppImage,
+      para:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    },
+  ];
   return (
     <div id="projects-page">
       <h1>Projects</h1>
-      <ConstructionNotice />
-      <br />
-      <p>
-        Please return to this page after <span>23/08/2021</span>.
-      </p>
+      {projectArray.map((item, i) => (
+        <div className="project-info-container" key={i + 1}>
+          <div className="column-1">
+            <img src={item.image} />
+          </div>
+          <div className="column-2" id={`item-${item.id}`}>
+            <h3>{item.name}</h3>
+            <p>{item.para}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
